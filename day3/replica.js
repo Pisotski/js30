ef4b89b23aed00ee1bc3c4d56733951e99c48717
Poisk <vpisotski@gmail.com>
2020-05-01 10:12:10 -0700

    started day 3. Have chosen a appropriate color.

---
+++ day3/replica.js
@@ -0,0 +1,33 @@
+// today, since there is no specific assignment. I want to replicate Wes Bos's page as precise as possible. I want to place every element on it's place as precise as possible.
+
+// goals:
+  // to learn and understand css varialbes
+    // declaration
+      // CODE:
+    // usage
+      //CODE:
+    // changing through css if that's possible
+      //CODE:
+    // changing through js
+      //CODE:
+    // deletion
+      //CODE:
+  // repeat a precise object placement. not flexbox
+  // learn more css
+
+  // steps:
+  // create a blue page
+  // place 3 elements
+    // h2 text bold
+    // h3 text with 3 range input
+    // a rectangle on top of the page that equals to 50% of given space
+    // place a random picture on top of a rectangle described above.
+  // bind a picture property with a certain range button
+  // using range input change a picture spacing, then blur
+  // learn how to call for a color palatte
+  // change a rectangle background color while color on the paallete is chosen
+  // bind change color input range to the palatte
+
+var body = document.querySelector('body');
+console.log(`I'm sexy and I know it ${body}`)
+
