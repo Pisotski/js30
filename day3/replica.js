be10eab95906af5d65e3c39f72ab90b24e1550d5
Poisk <vpisotski@gmail.com>
2020-05-01 12:09:35 -0700

    set up sutable HTML elements. set up positioning and colors in CSS

--- day3/replica.js
+++ day3/replica.js
@@ -19,13 +19,14 @@
   // create a blue page
   // place 3 elements
     // h2 text bold
+    // choose correct font and sizing as on video
     // h3 text with 3 range input
     // a rectangle on top of the page that equals to 50% of given space
     // place a random picture on top of a rectangle described above.
   // bind a picture property with a certain range button
   // using range input change a picture spacing, then blur
   // learn how to call for a color palatte
-  // change a rectangle background color while color on the paallete is chosen
+  // change a rectangle background color while color on the palatte is chosen
   // bind change color input range to the palatte
 
 var body = document.querySelector('body');
