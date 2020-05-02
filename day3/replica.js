c8ce789e2b77e63168837e2bfb716228813e805c
Poisk <vpisotski@gmail.com>
2020-05-02 14:31:32 -0700

    js past is over. Switching back to CSS

--- day3/replica.js
+++ day3/replica.js
@@ -25,10 +25,61 @@
     // place a random picture on top of a rectangle described above.
   // bind a picture property with a certain range button
   // using range input change a picture spacing, then blur
-  // learn how to call for a color palatte
-  // change a rectangle background color while color on the palatte is chosen
-  // bind change color input range to the palatte
+    // create an 3 event listeners that tracks when mouse is clicked for each input
 
-var body = document.querySelector('body');
-console.log(`I'm sexy and I know it ${body}`)
+const imagePadding = document.querySelector(`.frame`);
+const image = document.querySelector(`.frame img`);
+
+  // create 3 helper functions which will be invoked when related input element is clicked
+const imageInputs = [].slice.call(document.querySelectorAll('input'))
+
+const handleUpdate = function(e) {
+  const value = this.value;
+  switch (this.dataset.type) {
+  case 'spacing':
+    const perc = `${value}%`
+    image.style.top = perc;
+    image.style.left = perc;
+    break;
+  case 'blur':
+    image.style.filter  = `blur(${value}px)`;
+    break;
+  case 'base-color':
+    document.documentElement.style.setProperty('--colorful', value)
+    break;
+  }
+}
+
+imageInputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+imageInputs.forEach(input => input.addEventListener('change', handleUpdate))
+
+// const spacingInput = document.querySelector(`input[data-type='spacing']`);
+// const blurInput = document.querySelector(`input[data-type='blur']`);
+// const baseColorInput = document.querySelector(`input[data-type='base-color']`);
+
+// const spacingUpdate = function(e) {
+//   const spaceController = `${this.value}%`;
+//   image.style.top = spaceController;
+//   image.style.left = spaceController;
+// }
+// const blurUpdate = function(e) {
+//   const blurController = `${this.value}`;
+//   image.style.filter = `blur(${blurController}px)`;
+// }
+//   // learn how to call for a color palatte
+//   // ANSWER: <input type="color">
+
+// const baseColorUpdate = function(e) {
+//   const chosenColor = this.value;
+//   // change a rectangle background color while color on the palatte is chosen
+//   // bind change color input range to the palatte
+//   document.documentElement.style.setProperty('--colorful', chosenColor)
+// }
+
+// spacingInput.addEventListener('input', spacingUpdate);
+// blurInput.addEventListener('input', blurUpdate);
+// baseColorInput.addEventListener('input', baseColorUpdate);
+
+
+
 
