16798027d7a6593ce7da93b0b04203224053a3a3
Poisk <vpisotski@gmail.com>
2020-05-14 17:44:45 -0700

    complete day 10.

--- day10/holdShiftToMultipleCheckboxes.js
+++ day10/holdShiftToMultipleCheckboxes.js
@@ -1,4 +1,3 @@
-document.body.innerHTML = 'hi';
 
 // this time instead of flexbox i'll try to use table.
 // table content: 10 rows 2 colomns
@@ -10,7 +9,6 @@ document.body.innerHTML = 'hi';
 // create a table 10X2.
 //    first element of each row - input tag with type checkbox
 //    second - lorem ipsum
-// a second line in a table has to be twice as thick as all others
 // shadow to right low corner
 // create function that when checkbox is clicked will cross out a correctoding text element
 // keep track of is any of the checkboxes is already clicked
@@ -18,3 +16,53 @@ document.body.innerHTML = 'hi';
 // cross out all element in between selected once
 // make sure selection work in both ways, from top to bottom and from bottom to top.
 // ADDED FUNCTIONALITY: on click anywhere outside the table, all checkboxed should be deselected
+
+
+const mail = document.querySelector('table');
+// it seems like table cells in rows selection might be really helpfull to solve onwards\backwards selection problem
+// in this case there is no need to make any calculations, just pure selection;
+const letters = [...mail.rows];
+
+const crossOutLetter = function(e) {
+  // get text from selected row
+  // this.lastChild.previousElementSibling.innerHTML
+  const isCrossedOut = letters.some(letter => letter.classList.contains('crossed-out'));
+  this.classList.toggle('crossed-out');
+
+  if( isCrossedOut ) {
+
+    var crossingOut = false;
+    var hasStarted = false;
+
+    letters.forEach((letter, ind) => {
+      const decorated = letter.classList.contains('crossed-out');
+      if( crossingOut ) {
+        letter.classList.add('crossed-out');
+        letter.querySelector('input').checked = true;
+      }
+      if( !hasStarted && decorated ) {
+        hasStarted = true;
+        crossingOut = true;
+      } else if( hasStarted && decorated ) {
+        crossingOut = false;
+      }
+    })
+  }
+}
+
+// interesting workaround clicks outside of specified element
+
+letters.forEach( letter => {
+  letter.addEventListener('change', crossOutLetter);
+})
+
+document.addEventListener('click', function(event) {
+  const isClickInside = mail.contains(event.target);
+
+  if (!isClickInside) {
+    letters.map(letter => {
+      letter.classList.remove('crossed-out');
+      letter.querySelector('input').checked = false;
+    })
+  }
+});
