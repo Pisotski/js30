7a522fab4955720dde567a9cd4b2faba27dab21e
Poisk <vpisotski@gmail.com>
2020-05-16 12:34:34 -0700

    read some information about HTML5 customization. Made a plan and started PLAY|PAUSE button

--- day11/customHTMLVideoPlayer.js
+++ day11/customHTMLVideoPlayer.js
@@ -1,1 +1,17 @@
-document.body.innerHTML = 'LEVOI TUDOI<===, PRAVOI SYUDOI===>';
+// OBJECTIVE HTML5 videoplayer customization
+// create objects:
+  //  Play/pause button
+  //  Stop button
+  //  Rewind 10s button
+  //  Move forward 25s button
+  //  Volume up/down progress bar
+  //  Video progress bar. timeline
+// align objects as needed
+//  FUNCTIONALITY
+  //  Play/pause button
+  //  on click anywhere on the video object plays and stops the video
+  //  on press space button do the same
+  //  toggle SVGs icons for play/plase
+  //  on hover
+    //  make SVG a bit bigger
+    //  highlight a round background
