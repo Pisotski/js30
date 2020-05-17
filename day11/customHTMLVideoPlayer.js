fd2c7db34bc0bcb4ad0d17ff55c7d4c28ab85154
Poisk <vpisotski@gmail.com>
2020-05-16 20:30:22 -0700

    play/pause funcionality onclick, on spacebar press anywhere, onclick anywhere on the videoplayer not much done. Was reading alot

--- day11/customHTMLVideoPlayer.js
+++ day11/customHTMLVideoPlayer.js
@@ -11,7 +11,46 @@
   //  Play/pause button
   //  on click anywhere on the video object plays and stops the video
   //  on press space button do the same
-  //  toggle SVGs icons for play/plase
+  //  toggle SVGs icons for play/plause
   //  on hover
     //  make SVG a bit bigger
     //  highlight a round background
+
+const videoContainer = document.querySelector('#video-container');
+const video = document.querySelector('video');
+const videoControls = document.querySelector('#video-controls');
+const playPause = document.querySelector('#playpause');
+const stop = document.querySelector('#stop');
+
+
+video.controls = false;
+video.muted = true;
+
+const handlePlayPause = function(e) {
+
+  video.paused || video.ended ? video.play() : video.pause();
+  return;
+};
+
+const keyRouter = function(e) {
+
+  switch (e.keyCode) {
+    case ( 32 && document.activeElement !== playPause ) :
+        e.preventDefault();
+        handlePlayPause();
+    break;
+  }
+
+  return;
+}
+
+const handleStop = function(e) {
+  console.log('clckd')
+
+  return;
+}
+
+playPause.addEventListener('click', handlePlayPause);
+video.addEventListener('click', handlePlayPause);
+document.addEventListener('keydown', keyRouter);
+stop.addEventListener('click', handleStop);
