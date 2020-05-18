ff2573fa17ff58acb3558112c8fcf6e403ecc190
Poisk <vpisotski@gmail.com>
2020-05-18 13:42:05 -0700

    finally get a good result of play/pause symbols transition

--- day11/customHTMLVideoPlayer.js
+++ day11/customHTMLVideoPlayer.js
@@ -11,31 +11,42 @@
   //  Play/pause button
   //  on click anywhere on the video object plays and stops the video
   //  on press space button do the same
-  //  toggle SVGs icons for play/plause
+  //  toggle icons for play/plause on hover
   //  on hover
-    //  make SVG a bit bigger
-    //  highlight a round background
+    //  highlight a background
 
 const videoContainer = document.querySelector('#video-container');
 const video = document.querySelector('video');
 const videoControls = document.querySelector('#video-controls');
-const playPause = document.querySelector('#playpause');
+const playPause = document.querySelector('.btn');
 const stop = document.querySelector('#stop');
-
 
 video.controls = false;
 video.muted = true;
 
 const handlePlayPause = function(e) {
 
+  togglePlayPause()
   video.paused || video.ended ? video.play() : video.pause();
   return;
 };
 
+const togglePlayPause = function(){
+
+  if (playPause.classList.contains('play')){
+    playPause.classList.remove('play');
+    playPause.classList.add('pause');
+  } else {
+    playPause.classList.remove('pause');
+    playPause.classList.add('play');
+  }
+  return;
+}
 const keyRouter = function(e) {
 
+  console.log(document.activeElement === playPause)
   switch (e.keyCode) {
-    case ( 32 && document.activeElement !== playPause ) :
+    case 32 :
         e.preventDefault();
         handlePlayPause();
     break;
@@ -53,4 +64,4 @@ const handleStop = function(e) {
 playPause.addEventListener('click', handlePlayPause);
 video.addEventListener('click', handlePlayPause);
 document.addEventListener('keydown', keyRouter);
-stop.addEventListener('click', handleStop);
+// stop.addEventListener('click', handleStop);
