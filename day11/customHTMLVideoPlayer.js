3ab6f2bbfc56d128dcf325632c1ee98126c29c1b
Poisk <vpisotski@gmail.com>
2020-05-20 12:58:38 -0700

    clean up

--- day11/customHTMLVideoPlayer.js
+++ day11/customHTMLVideoPlayer.js
@@ -25,69 +25,114 @@ const volume = document.querySelector('.volume-progress');
 const volumeContainer = document.querySelector('.volume-container');
 const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress-bar');
+const forwardRewind = document.querySelector('.forward-container')
+const backwardRewind = document.querySelector('.backward-container')
+
 
 video.controls = false;
 video.muted = true;
-mute.style.backgroundColor = 'sandybrown'
+mute.style.backgroundColor = 'sandybrown';
 
-const handlePlayPause = function(e) {
+const handlePlayPause = function( e ) {
 
   togglePlayPause();
   video.paused || video.ended ? video.play() : video.pause();
   return;
+
 };
 
-const togglePlayPause = function(){
+const togglePlayPause = function() {
 
-  if (playPause.classList.contains('play')){
-    playPause.classList.remove('play');
-    playPause.classList.add('pause');
+  if ( playPause.classList.contains( 'play' ) ) {
+    playPause.classList.remove( 'play' );
+    playPause.classList.add( 'pause' );
   } else {
-    playPause.classList.remove('pause');
-    playPause.classList.add('play');
+    playPause.classList.remove( 'pause' );
+    playPause.classList.add( 'play' );
   }
   return;
-}
 
-const handleStop = function(e) {
+};
+
+const handleStop = function( e ) {
 
   video.currentTime = 0;
   video.pause();
-  stop.classList.toggle('stop-active')
+  stop.classList.toggle( 'stop-active' );
   return;
-}
 
-const handleMute = function(){
+};
+
+const handleMute = function( e ) {
 
   const isMuted = video.muted;
   video.muted = isMuted ? false : true;
   mute.style.backgroundColor = isMuted ? 'transparent' : 'var(--highlight-color)';
-}
 
-const handleProgress = function() {
+};
+
+const handleProgress = function( e ) {
 
   progressBar.value = video.currentTime;
-  progress.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
-}
+  progress.style.width = Math.floor( ( video.currentTime / video.duration ) * 100 ) + '%';
+
+};
 
 const handleRewind = function(e) {
+  
+  if(this !== progressBar) {
+
+    const isForward = this === forwardRewind;
+    const step = isForward ? 25 : -10;
+    const isOut = outOfTimeFrame(step);
+
+    if( isOut ) {
+
+      isForward ? video.currentTime = video.duration : video.currentTime = 0;
+      return;
+
+    }
+
+    video.currentTime += step;
+    return;
+
+    }
 
   const pos = (e.pageX  - this.offsetLeft) / this.offsetWidth;
   video.currentTime = pos * video.duration;
+  return;
+
+}
+
+const outOfTimeFrame = function(seconds) {
+
+  const futureTimeMark = video.currentTime + seconds;
+
+  if( seconds < 0 ) {
+
+    return futureTimeMark < 0
+
+  } else {
+
+    return futureTimeMark > video.duration
+
+  }
+
 }
 
 const handleVolume = function(e) {
   
   video.muted = false;
   mute.style.backgroundColor = 'transparent';
-  const pos = (e.pageX  - this.offsetLeft) / this.offsetWidth ;
+  const pos = (e.pageX  - this.offsetLeft) / this.offsetWidth;
   volume.value = pos;
   video.volume = pos;
+
 }
 
-const keyRouter = function(e) {
+const keyRouter = function( e ) {
 
-  switch (e.keyCode) {
+  switch ( e.keyCode ) {
 
     case 32 :
       e.preventDefault();
@@ -106,16 +151,19 @@ const keyRouter = function(e) {
     }
 
   return;
+
 }
 
-   document.addEventListener('keydown', keyRouter);
-      video.addEventListener('loadedmetadata', () => progressBar.setAttribute('max', video.duration));
-      video.addEventListener('loadedmetadata', () => volume.value = 0.5);
-      video.addEventListener('timeupdate', handleProgress);
-      video.addEventListener('click', handlePlayPause);
-  playPause.addEventListener('click', handlePlayPause);
-       stop.addEventListener('click', handleStop);
-       mute.addEventListener('click', handleMute)
-progressBar.addEventListener('click', handleRewind);
-     volume.addEventListener('click', handleVolume);
-     volume.addEventListener('mousedown mouseup', handleVolume)
+         video.addEventListener('loadedmetadata', () => progressBar.setAttribute('max', video.duration));
+      document.addEventListener('keydown', keyRouter);
+         video.addEventListener('loadedmetadata', () => volume.value = 0.5);
+         video.addEventListener('timeupdate', handleProgress);
+         video.addEventListener('click', handlePlayPause);
+     playPause.addEventListener('click', handlePlayPause);
+          stop.addEventListener('click', handleStop);
+          mute.addEventListener('click', handleMute)
+   progressBar.addEventListener('click', handleRewind);
+ forwardRewind.addEventListener('click', handleRewind);
+backwardRewind.addEventListener('click', handleRewind);
+        volume.addEventListener('click', handleVolume);
+        volume.addEventListener('mousedown mouseup', handleVolume);
